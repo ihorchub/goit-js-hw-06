5d1f1800743f26d772e19270cc81c39d83311877
Ihor Chubov <ihor.chub@gmail.com>
2022-09-24 22:18:14 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -34,51 +34,28 @@ const refs = {
 };
 const boxes = document.querySelector('#boxes');
 
-// function createBoxes(amount) {
-//   function handlerInput() {
-//     amount = refs.input.value;
-//   }
-//   refs.input.addEventListener('input', handlerInput);
-//   function handlerCreate() {
-//     for (let i = 1; i <= amount; i += 1) {
-//       let newDiv = document.createElement('div');
-//       let sizeOfDiv = (i - 1) * 10 + 30;
-//       newDiv.style.width = `${sizeOfDiv}px`;
-//       newDiv.style.height = `${sizeOfDiv}px`;
-//       newDiv.style.backgroundColor = getRandomHexColor();
-//       boxes.append(newDiv);
-//     }
-//   }
-//   refs.buttonCreate.addEventListener('click', handlerCreate);
-// }
-
-let amount = 0;
-
-function handlerInput() {
-  amount = refs.input.value;
-}
-
-function handlerCreate() {
-  for (let i = 1; i <= amount; i += 1) {
-    let newDiv = document.createElement('div');
-    let sizeOfDiv = (i - 1) * 10 + 30;
-    newDiv.style.width = `${sizeOfDiv}px`;
-    newDiv.style.height = `${sizeOfDiv}px`;
-    newDiv.style.backgroundColor = getRandomHexColor();
-    boxes.append(newDiv);
+function createBoxes(amount) {
+  function handlerInput() {
+    amount = refs.input.value;
   }
-}
-
-function handlerDestroy() {
-  boxes.innerHTML = '';
-}
-
-function createBoxes() {
   refs.input.addEventListener('input', handlerInput);
+  function handlerCreate() {
+    for (let i = 1; i <= amount; i += 1) {
+      let newDiv = document.createElement('div');
+      let sizeOfDiv = (i - 1) * 10 + 30;
+      newDiv.style.width = `${sizeOfDiv}px`;
+      newDiv.style.height = `${sizeOfDiv}px`;
+      newDiv.style.backgroundColor = getRandomHexColor();
+      boxes.append(newDiv);
+    }
+  }
   refs.buttonCreate.addEventListener('click', handlerCreate);
 }
 
 function destroyBoxes() {
+  function handlerDestroy() {
+    boxes.innerHTML = '';
+  }
   refs.buttonDestroy.addEventListener('click', handlerDestroy);
 }
 
